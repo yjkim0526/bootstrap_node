a15eb1d7e335389f07ebdaafff8a0a924413d57c
yujin kim <koiforever0526@gmail.com>
2025-01-23 08:58:36 +0900

    app edit

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const port = 3300;
 const app = express();
 
+require("dotenv").config();
+app.set("port", process.env.PORT || 3500);
+// console.log(process.env.PORT);
+
+//app.set("public", "./public");
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`서버 실행 : http://localhost:${port}`);
+app.listen(app.get("port"), () => {
+  //console.log(`서버 실행 : http://localhost:${app.get("port")}`);
+  console.log(`Example app listening on port ${app.get("port")}`);
 });
